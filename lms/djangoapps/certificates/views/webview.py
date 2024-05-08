0a3a14c42fd351182385e1b6c10a805c7046ba1f
Xeno Kovah <xeno@ost.fyi>
2024-05-08 17:19:16 -0400

    Testing modifying cert content from the edx-platform side

--- lms/djangoapps/certificates/views/webview.py
+++ lms/djangoapps/certificates/views/webview.py
@@ -144,7 +144,7 @@ def _update_certificate_context(context, course, course_overview, user_certifica
 
     # Translators:  This text fragment appears after the student's name (displayed in a large font) on the certificate
     # screen.  The text describes the accomplishment represented by the certificate information displayed to the user
-    context['accomplishment_copy_description_full'] = _("successfully completed, received a passing grade, and was "
+    context['accomplishment_copy_description_full'] = _("successfully completed this class, and was "
                                                         "awarded this {platform_name} {certificate_type} "
                                                         "Certificate of Completion in ").format(
         platform_name=platform_name,
